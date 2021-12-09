526a771fbf14020e2194f3453dc46a76e42fe5ba
Georgi Yankov <georgi.st.yankov@gmail.com>
2021-12-09 08:39:26 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // All declared variables here can be used in app.component.html
+  title = 'Tour Of Heroes';
+}
---
+++ src/app/hero.ts
@@ -0,0 +1,15 @@
+export interface Hero {
+  id: number;
+  name: string;
+}
+
+/** This variant also works */
+
+// export class Hero {
+
+//   constructor(
+//     public id: number,
+//     public name: string
+//   ) {  }
+
+// }
